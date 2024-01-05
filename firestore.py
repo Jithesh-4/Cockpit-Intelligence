f6a7ee1d5c3b29f3631edc0ea68b9cfd9fd53e55
Jithesh S <125364788+Jithesh-4@users.noreply.github.com>
2024-01-05 14:17:00 +0530

    Update firestore.py

--- firestore.py
+++ firestore.py
@@ -1,6 +1,14 @@
 import time
-#import pyrebase
+import firebase_admin
 import RPi.GPIO as GPIO
+import cv2
+import dlib
+import imutils
+import random
+from imutils import face_utils
+from scipy.spatial import distance as dist
+import RPi.GPIO as GPIO
+from threading import Thread
 from threading import Thread
 from firebase_admin import credentials, initialize_app, db
 
@@ -26,6 +34,29 @@ GPIO.setup(brake_pin1, GPIO.IN)
 GPIO.setup(brake_pin2, GPIO.IN)
 GPIO.setup(brake_pin3, GPIO.IN)
 
+#
+FACIAL_LANDMARK_PREDICTOR = "/home/pi/Desktop/rasberry-pi-iot-car/shape_predictor_68_face_landmarks.dat"  # path to dlib's pre-trained facial landmark predictor
+MINIMUM_EAR = 0.2    # Minimum EAR for both the eyes to mark the eyes as open
+MAXIMUM_FRAME_COUNT = 10
+
+#Initializations
+faceDetector = dlib.get_frontal_face_detector()     # dlib's HOG based face detector
+landmarkFinder = dlib.shape_predictor(FACIAL_LANDMARK_PREDICTOR)  # dlib's landmark finder/predcitor inside detected face
+webcamFeed = cv2.VideoCapture(0)
+
+# Finding landmark id for left and right eyes
+(leftEyeStart, leftEyeEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rightEyeStart, rightEyeEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+def eye_aspect_ratio(eye):
+    p2_minus_p6 = dist.euclidean(eye[1], eye[5])
+    p3_minus_p5 = dist.euclidean(eye[2], eye[4])
+    p1_minus_p4 = dist.euclidean(eye[0], eye[3])
+    ear = (p2_minus_p6 + p3_minus_p5) / (2.0 * p1_minus_p4)
+    return ear
+
+EYE_CLOSED_COUNTER = 0
+
 
 
 cred = credentials.Certificate("/home/pi/Desktop/pmain/predictive-maintainence-1841d-firebase-adminsdk-oejc1-37178fae8b.json")
@@ -37,6 +68,47 @@ print("Send Data to Firebase Using Raspberry Pi")
 print("----------------------------------------")
 print()
 
+
+def earCalculation():
+    earCalculation.ear_val = 0
+    while True:
+        (status, image) = webcamFeed.read()
+        image = imutils.resize(image, width=800)
+        grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        faces = faceDetector(grayImage, 0)
+
+        for face in faces:
+            faceLandmarks = landmarkFinder(grayImage, face)
+            faceLandmarks = face_utils.shape_to_np(faceLandmarks)
+
+            leftEye = faceLandmarks[leftEyeStart:leftEyeEnd]
+            rightEye = faceLandmarks[rightEyeStart:rightEyeEnd]
+
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+
+            ear = (leftEAR + rightEAR) / 2.0
+            earCalculation.ear_val = ear
+
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+
+            cv2.drawContours(image, [leftEyeHull], -1, (255, 0, 0), 2)
+            cv2.drawContours(image, [rightEyeHull], -1, (255, 0, 0), 2)
+
+            if ear < MINIMUM_EAR:
+                EYE_CLOSED_COUNTER += 1
+            else:
+                EYE_CLOSED_COUNTER = 0
+
+            cv2.putText(image, "EAR: {}".format(round(ear, 1)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+            if EYE_CLOSED_COUNTER >= MAXIMUM_FRAME_COUNT:
+                cv2.putText(image, "Drowsiness", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        cv2.imshow("Frame", image)
+        cv2.waitKey(1)
 
 
 def parametersCalculation():
@@ -76,3 +148,7 @@ def parametersCalculation():
 
         db.child("sensor-values").set(data)
         time.sleep(2)
+
+"""if _name_ == '_main_':
+    Thread(target = earCalculation).start()
+    Thread(target = parametersCalculation).start()"""
